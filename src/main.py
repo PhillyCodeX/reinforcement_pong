f0e75f76b02f8bfbd75b69ac290188b009f93ab2
Philipp Paraguya <philippparaguya@gmail.com>
2019-05-23 16:35:37 +0200

    added basic main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
+import src.game_objects as go
+import src.strategies as strat
+
 def main():
-    new_game = Game()
 
+    print('Let\'s Play Pong!!!')
+    new_game = go.Game()
+
+    new_game.setPlayers()
+    print('Players are set!')
+    print('Player 1, Welcome!: '+new_game.player1.name)
+    print('Player 2, Welcome!: '+new_game.player2.name)
     
-
 
 if __name__ == '__main__': 
     main() 
