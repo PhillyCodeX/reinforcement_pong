b24df80329a668dc4c7b5184d68e4c2d8bcfe58e
Philipp Paraguya <philippparaguya@gmail.com>
2019-05-23 18:05:29 +0200

    added pygame but not yet finished for manual strategy

--- src/strategies.py
+++ src/strategies.py
@@ -3,20 +3,26 @@ import abc
 class Strategy(metaclass=abc.ABCMeta):
     
    @abc.abstractmethod
-   def next_pos(self, p_pos):
+   def next_pos(self, p_pos, p_dir_up):
        pass 
        
 class DumbStrat(Strategy):
     
-    def next_pos(self, pos):
+    def next_pos(self, p_pos, p_dir_up):
         pass
 
 class ManualStrat(Strategy):
-    
-    def next_pos(self, pos):
-        pass
+    inputMap = [False, False] 
+
+    def next_pos(self, p_pos, p_dir_up):
+        if p_dir_up is None:
+            return p_pos
+        elif p_dir_up == True:
+            return p_pos+10
+        else:
+            return p_pos-10
 
 class ReinforcedStrat(Strategy):
     
-    def next_pos(self, pos):
+    def next_pos(self, p_pos, p_dir_up):
         pass
