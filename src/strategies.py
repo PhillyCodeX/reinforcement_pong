dc27507263e39c2f84b60d37bdad3797e485bd56
Philipp Paraguya <philippparaguya@gmail.com>
2019-05-23 18:49:33 +0200

    implemented internal game logic (not tested yet)

--- src/strategies.py
+++ src/strategies.py
@@ -3,26 +3,26 @@ import abc
 class Strategy(metaclass=abc.ABCMeta):
     
    @abc.abstractmethod
-   def next_pos(self, p_pos, p_dir_up):
+   def next_pos(self, p_pos, p_vel, p_dir_up):
        pass 
        
 class DumbStrat(Strategy):
     
-    def next_pos(self, p_pos, p_dir_up):
+    def next_pos(self, p_pos, p_vel, p_dir_up):
         pass
 
 class ManualStrat(Strategy):
     inputMap = [False, False] 
 
-    def next_pos(self, p_pos, p_dir_up):
+    def next_pos(self, p_pos, p_vel, p_dir_up):
         if p_dir_up is None:
             return p_pos
         elif p_dir_up == True:
-            return p_pos+10
+            return p_pos+p_vel
         else:
-            return p_pos-10
+            return p_pos-p_vel
 
 class ReinforcedStrat(Strategy):
     
-    def next_pos(self, p_pos, p_dir_up):
+    def next_pos(self, p_pos, p_vel, p_dir_up):
         pass
