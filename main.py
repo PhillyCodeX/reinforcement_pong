d5bd35ebdba9dabc54a54d5b263d1d4e627b9418
Philipp Paraguya <philippparaguya@gmail.com>
2019-05-24 11:57:08 +0200

    added new game functionality and a 10 point goal

--- main.py
+++ main.py
@@ -2,18 +2,39 @@ import src.game_objects as go
 import src.strategies as strat
 
 def main():
-
-    print('Let\'s Play Pong!!!')
+    want_new_players = True
+    want_new_game = True
     new_game = go.Game()
 
-    new_game.setPlayers()
-    print('Players are set!')
-    print('Player 1, Welcome!: '+new_game.player1.name)
-    print('Player 2, Welcome!: '+new_game.player2.name)
-    
-    new_game.play()
+    while want_new_game:
+        want_new_game = False
+        print('Let\'s Play Pong!!!')
+        
+        if want_new_players:
+            want_new_players = False
+            new_game.setPlayers()
+            print('Players are set!')
+            print('Player 1, Welcome!: '+new_game.player1.name)
+            print('Player 2, Welcome!: '+new_game.player2.name)
+        
+        new_game.play()
+        winner = new_game.winner
+        
+        print("Game finished!!!")
+        print(winner.name + " won! Congratulations")
+        
+        user_input = input("Do you want to play a new game?[Yes/No] ")
 
-    print("Game finished!!!")
+        if user_input == "Yes":
+            want_new_game = True
+
+            user_input = input("Do you want to set new players?[Yes/No] ")
+
+            if user_input == "Yes":
+                want_new_players = True
+            elif user_input == "No":
+                new_game.player1.points = 0
+                new_game.player2.points = 0
     
 if __name__ == '__main__': 
     main() 
